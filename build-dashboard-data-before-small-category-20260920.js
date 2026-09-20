495a89b0d6fd407e84761a054b9fa6485cbe8a79
xinbo2324-cyber <xinbo2324-cyber@users.noreply.github.com>
2026-09-20 16:07:16 +0800

    Use small category and rank in dashboard

--- build-dashboard-data-before-small-category-20260920.js
+++ build-dashboard-data-before-small-category-20260920.js
@@ -58,8 +58,6 @@ function mapRow(source, salesMonths, snapshotOnly) {
   });
   item['商品ASIN'] = item['竞品ASIN'] || '';
   item['竞品ASIN'] = item['父ASIN'] || item['竞品ASIN'] || '';
-  item['品类'] = item['小类'] || item['品类'] || '';
-  item['大类排名'] = item['小类排名'] || item['大类排名'] || '';
   item['版型'] = normalizeFit(item['版型']);
   item['闭合方式'] = normalizeClosure(item['闭合方式']);
   item['价格梯度'] = priceBand(item['Buybox价格']);
